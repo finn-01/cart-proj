4d9c42362dc3ee1ef97a48a2011c7f85a903b081
finn-01 <nguyensam576@gmail.com>
2022-01-16 21:33:27 +0700

    set up 2

--- src/context.js
+++ src/context.js
@@ -1,6 +1,6 @@
 import React, { useState, useContext, useReducer, useEffect } from "react";
 
-import { cartItems } from "./data";
+import cartItems from "./data";
 import reducer from "./reducer";
 
 const url = "https://course-api.com/react-useReducer-cart-project";
